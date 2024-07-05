b7a842a024d1a444bc922ac64b0b1fcf70c1f67f
javascript_tarea3.git <camilapalacios112@gmail.com>
2024-07-05 11:52:45 -0400

    modifique error

--- sample.py
+++ sample.py
@@ -1,44 +1,51 @@
-
-while True:
-    print('Seleccione el proceso que desea aplicar')
-    print('1: Introduzca los puntos de valoración y los comentarios ')
-    print('2: Comprueba los resultados obtenidos hasta ahora')
-    print('3: Terminación.' )
-    num = input()
-    
-    if num.isdecimal():
-        num=int(num)
-        
-        if num == 1:
-            while True:
-                print('Por Favor, introduzca una puntuación en una escala de 1 a 5')
-                punto = input()
-                if punto.isdecimal():
-                 punto = int(punto)
-                if punto <= 1 or  punto > 5:
-                    print('Indique una escala de 1 a 5: ')
-                    punto = input()
-                else:
-                    print('Introduzca sus comentarios: ')
-                    comentario = input()
-                    post = f'punto: {punto} comentario: {comentario}'
-                    with open("data.txt", 'a') as file_pc:
-                        file_pc.write(f'{post} \n')
-                    file_pc.close()
-                    break
-                
-                #si se introduce un valor no numerico(int)
+def introducir_puntos_y_comentarios():
+    while True:
+        print('Por favor, introduzca una puntuación en una escala de 1 a 5')
+        punto = input()
+        if punto.isdecimal():
+            punto = int(punto)
+            if punto <= 0 or punto > 5:
+                print('Indíquelo en una escala de 1 a 5')
             else:
-                print('Introduzca un numero')
-        elif num == 2:
-            print('Resultado hasta la fecha') 
-            read_file = open("data.txt", "r")
-            print(read_file.read())
-            read_file.close()
-        elif num == 3:
-            print('Terminación')
-            break
+                print('Introduzca sus comentarios.')
+                comment = input()
+                post = f'punto: {punto} comentario: {comment}'
+                with open("data.txt", 'a') as file_pc:
+                    file_pc.write(f'{post} \n')
+                break
         else:
-            print('Por favor, Introduzca de 1 a 3')
-    else:
-        print('Por favor, introduzca un número válido')
+            print('Introduzca los puntos de valoración como números')
+
+def comprobar_resultados():
+    print('Resultados hasta la fecha.')
+    with open("data.txt", "r") as read_file:
+        print(read_file.read())
+
+def terminar():
+    print('Terminación.')
+
+def main():
+    while True:
+        print('Seleccione el proceso que desea aplicar')
+        print('1: Introducir puntos de evaluación y comentarios')
+        print('2: Comprueba los resultados hasta ahora.')
+        print('3: Terminar.')
+        num = input()
+
+        if num.isdecimal():
+            num = int(num)
+
+            if num == 1:
+                introducir_puntos_y_comentarios()
+            elif num == 2:
+                comprobar_resultados()
+            elif num == 3:
+                terminar()
+                break
+            else:
+                print('Introduzca de 1 a 3')
+        else:
+            print('Introduzca de 1 a 3')
+
+if __name__ == "__main__":
+    main()
